a10377406efb445b4b162d227bf60b7197b1e493
Toni Cárdenas <toni@tcardenas.me>
2015-01-26 15:45:02 +0100

    Fix setup.py for PyPI.

--- setup.py
+++ setup.py
@@ -1,19 +1,42 @@
 #! /usr/bin/env python
 
-from setuptools import setup
-import sigdispatch
+import os
+import re
+from setuptools import setup, find_packages
+
+
+def get_version_from_init():
+    file = open(os.path.join(
+        os.path.dirname(__file__),
+        'sigdispatch',
+        '__init__.py'
+    ))
+
+    regexp = re.compile(r".*__version__ = '(.*?)'", re.S)
+    version = regexp.match(file.read()).group(1)
+    file.close()
+
+    return version
 
 setup(
     name='sigdispatch',
-    version=sigdispatch.__version__,
+    version=get_version_from_init(),
     description='A simple events library.',
     author='Tyba',
     author_email='toni@tyba.com',
     url='http://github.com/Tyba/sigdispatch',
-    packages=[
-        'sigdispatch',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
     ],
-    install_requires=[
-        'sphinxcontrib-napoleon',
-    ],
+    packages=find_packages(exclude=['docs', 'tests*']),
+    install_requires=[],
 )
